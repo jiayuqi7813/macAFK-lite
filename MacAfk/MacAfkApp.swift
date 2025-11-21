d7353d783e18a98875c0f9abbbc665c47cadf613
jiayuqi7813 <1783671926@qq.com>
2025-11-21 14:32:49 +0800

    Initial Commit

---
+++ MacAfk/MacAfkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacAfkApp.swift
+//  MacAfk
+//
+//  Created by Sn1waR on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MacAfkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacAfkTests/MacAfkTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacAfkTests.swift
+//  MacAfkTests
+//
+//  Created by Sn1waR on 11/21/25.
+//
+
+import Testing
+@testable import MacAfk
+
+struct MacAfkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
